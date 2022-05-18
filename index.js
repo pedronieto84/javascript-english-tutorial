9f7b234ce569f394dff3a1e069b97f902ff57c60
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-18 19:15:31 +0200

    Console log. And Javascript variables: let and const.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log('hello world')
+
+// const, let and variables
+
+// const is a type of variable that you can't change
+
+const name = 'Lie'
+console.log('My name is', name)
+
+// let is a type of variable that you can change
+
+let name2 = 'Natalie'
+name2 = 'Pedro'
+console.log('Your name is', name2)
+
+// var is like a let but it has some different behaviour over let in relation to the scope
+
