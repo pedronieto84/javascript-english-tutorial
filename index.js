07d9c0a20ed2ba50a14092ffadf2e4576deaaba5
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-21 12:46:11 +0200

    Javascript data types (2nd Part)

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const name = 'Lie' // Is a string  is text
-const name2= "Pedro"
+// PRIMITIVE DATA TYPES
 
+const name = 'Lie' // Is a string 
 
 const number = 12 // THis is a number
-const numberAsString = "12" // This is a String
 
 const boolean = false // Can be only true or False.
 
 const nullDataType = null // Reserves the space on memory byt there is nothing
 
+let exampleOfUndefined; // Example of a data that is undefined
+console.log(exampleOfUndefined)
+
+
+// Non PRIMITIVE DATA TYPES
 const object = {
     name: 'Lie',
     age: 33
 }
+
+function exampleOfFunction(){
+    console.log('i am a funciton')
+}
+
+exampleOfFunction()
+
 
 
 
